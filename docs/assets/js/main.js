fa94e287c969aa8d8bdddaa384f606e34013f1bd
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2020-04-09 14:00:15 -0500

    Added maps section.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,14 +1,33 @@
 "use strict";
 
 initPreloader();
+
 $(document).ready(function () {
-  feather.replace();
-  initNavbar();
-  initMobileMenu();
-  initSidebar();
-  initModals();
-  initScrollReveal();
-  initBackToTop();
-  initBackgroundImages();
-  initScrollToHash();
+
+    feather.replace();
+
+    initNavbar();
+
+    initMobileMenu();
+
+    initSidebar();
+
+    initModals();
+
+    initScrollReveal();
+
+    initBackToTop();
+
+    initBackgroundImages();
+
+    initScrollToHash();
+});
+
+// Using Express.js to map endpoints
+var express = require('express');
+var app = express();
+
+app.get('/mapa', function (req, res) {
+    console.log(__dirname);
+    res.sendFile('mapa.html', { root: __dirname });
 });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -23,3 +23,13 @@ $(document).ready(function () {
     initScrollToHash();
 
 })
+
+
+// Using Express.js to map endpoints
+const express = require('express');
+const app = express();
+
+app.get('/mapa', (req, res) => {
+    console.log(__dirname)
+    res.sendFile('mapa.html', { root: __dirname });
+});

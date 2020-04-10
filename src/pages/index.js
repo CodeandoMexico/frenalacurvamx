452f22f6e42304ab8be82390875f9e595260afbb
Guillermo D. Vicencio <momosolamente@gmail.com>
2020-04-10 02:13:30 -0500

    Added twitter

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import MediosOficiales from '../components/mediosOficiales'
 import Iniciativas from '../components/iniciativas'
+import Twitter from "../components/twitter"
 
 const IndexPage = () => (
     <Layout>
         <SEO title="Inicio" />
         <MediosOficiales />
         <Iniciativas />
+        <Twitter />
     </Layout>
 )
 export default IndexPage

c75845746091db25eb92e59cef3a040f8b553177
Guillermo D. Vicencio <momosolamente@gmail.com>
2020-04-09 22:01:12 -0500

    Updated project metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Frena la curva - México`,
+    description: `Guía de iniciativas ciudadanas frente al COVID19 para visibilizar, difundir y multiplicar el impacto de proyectos e iniciativas que han brotado en México`,
+    author: `Codeando México`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,12 +19,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `FrenaLaCurvaMX`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#E83F4E`,
+        theme_color: `#E83F4E`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,

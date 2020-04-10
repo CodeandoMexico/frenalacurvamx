40384d05c8f3a5f11d1cbda351e7491efc1b4f6a
Guillermo D. Vicencio <momosolamente@gmail.com>
2020-04-10 00:45:48 -0500

    Added Flags component and moved flag icons to static folder for convenience

---
+++ src/components/flags/flags.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Flags = () => (
+    <nav className="navbar is-fresh is-transparent no-shadow" role="navigation" ariaLabel="main navigation">
+        <div className="container">
+            <div className="navbar-end is-pulled-right">
+                <div className="navbar-brand navbar-flags">
+                    <p className="flags-item is-secondary">Paises actuando:</p>
+                    <a href="https://frenalacurva.net/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/spain.png"  alt="bandera de España" />
+                    </a>
+                    <a href="https://colombia.frenalacurva.net/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/colombia.png"  alt="bandera de Colombia" />
+                    </a>
+                    <a href="https://mexico.frenalacurva.net/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/mexico.png"  alt="bandera de México" />
+                    </a>
+                    <a href="https://costarica.frenalacurva.net/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/costarica.png"  alt="bandera de Costa Rica" />
+                    </a>
+                    <a href="http://widzimy.online/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/polonia.png"  alt="bandera de Polonia" />
+                    </a>
+                    <a href="https://ecuador.frenalacurva.net/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/ecuador.png"  alt="bandera de Ecuador" />
+                    </a>
+                    <a href="http://co-confines.fr/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/francia.png"  alt="bandera de Francia" />
+                    </a>
+                    <a href="http://www.seguraaonda.com.br/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/brasil.png"  alt="bandera de Brasil" />
+                    </a>
+                    <a href="http://www.acaestamos.uy/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/uruguay.png"  alt="bandera de Uruguay" />
+                    </a>
+                    <a href="http://argentina.frenalacurva.net/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/argentina.png"  alt="bandera de Argentina" />
+                    </a>
+                    <a href="http://frenalacurva.org.pe/" target="_blank" className="flags-item is-secondary" rel="noopener noreferrer">
+                        <img src="icons/peru.jpg"  alt="bandera de Perú" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+)
+
+export default Flags
--- src/components/header.js
+++ src/components/header.js
@@ -2,41 +2,27 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import Flags from './flags/flags'
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+    <header>
+        <Flags />
+        <div>
+            <h1>
+                <Link to="/" >
+                    {siteTitle}
+                </Link>
+            </h1>
+        </div>
+    </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+    siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 }
 
 export default Header

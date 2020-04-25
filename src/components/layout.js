dedf3da94f34ad71bdfef16992190a613f6e6474
Guillermo D. Vicencio <momosolamente@gmail.com>
2020-04-25 03:52:06 -0500

    Se agrega fb pixel

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,6 +46,21 @@ const Layout = ({ children }) => {
                     gtag('js', new Date());
                     gtag('config', 'UA-1803821-25');
                 `}</script>
+                <script>{`
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '544418153168223');
+                    fbq('track', 'PageView');
+                `}</script>
+                <noscript>{`
+                    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=544418153168223&ev=PageView&noscript=1" />
+                `}</noscript>
             </Helmet>
             {/* TODO: Enable preloader component with correct implementation */}
             {/* <Preloader /> */}

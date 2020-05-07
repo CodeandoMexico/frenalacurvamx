1c49ca2ddb33880ffe187cbfdb5861ebc0bfb20a
Guillermo D. Vicencio <momosolamente@gmail.com>
2020-05-06 21:37:48 -0500

    Se agrega eslint prettier y lint-staged Se agrega husky and lint-staged Archivos formateados con prettier updated eslint and prettier config fixed lint errors updated eslint config Archivos autocorregidos con npm run lint Se agrega npm script para usar eslint --fix Se agregan eslint react rules

--- src/pages/quienes-somos.jsx
+++ src/pages/quienes-somos.jsx
@@ -1,24 +1,20 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const QuienesSomosPage = () => (
-    <Layout>
-        <SEO title="Quiénes somos" />
-        <section class="hero is-large">
-            <div className="hero-body">
-                <div class="container">
-                    <h1 class="title">Quiénes somos:</h1>
-                    <h2 class="subtitle">
-                        Próximamente...
-                    </h2>
-                </div>
-            </div>
-        </section>
-    </Layout>
+  <Layout>
+    <SEO title="Quiénes somos" />
+    <section className="hero is-large">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Quiénes somos:</h1>
+          <h2 className="subtitle">Próximamente...</h2>
+        </div>
+      </div>
+    </section>
+  </Layout>
 )
-
 
 export default QuienesSomosPage

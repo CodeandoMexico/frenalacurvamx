7580c95bb8ed8936bde04fab9f340d03c708a073
Guillermo D. Vicencio <momosolamente@gmail.com>
2020-05-07 01:43:48 -0500

    Archivos renombrados con extensión jsx

--- src/pages/mapa.jsx
+++ src/pages/mapa.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ModerationSection from '../components/moderationSection/moderationSection'
 import UseMapSection from '../components/useMapSection/useMapSection'
-import MapSection from '..//components/mapSection/mapSection'
+import MapSection from '../components/mapSection/mapSection'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
